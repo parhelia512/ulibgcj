c6c546b91b7aa5f4a3222d65de4f91427fd89aaa
Joel Dice <git@seibutsu.mailsnare.net>
2006-06-30 11:30:55 -0600

    made GC-specific code conditional on HAVE_BOEHM_GC

--- ulibgcj/test/Hello.cpp
+++ ulibgcj/test/Hello.cpp
@@ -1,12 +1,16 @@
 #include "config.h"
 #include "gcj/cni.h"
 #include "Hello.h"
+#ifdef HAVE_BOEHM_GC
 #include "gc.h"
+#endif
 
 int
 main()
 {
+#ifdef HAVE_BOEHM_GC
   GC_set_max_heap_size(24 * 1024 * 1024);
+#endif
 
   JvCreateJavaVM(0);
   JvAttachCurrentThread(NULL, NULL);
